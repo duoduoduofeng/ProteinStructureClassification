52c5d89eb7374a52418b5dc33318619cd149cdb9
FENG DUO <duoduo@Lucys-Macbook.local>
2024-01-15 19:52:41 -0800

    Normal the code.

--- src/main.py
+++ src/main.py
@@ -12,24 +12,26 @@ from model.process import *
 
 
 if __name__ == "__main__":
-    ### 1. Training
-    # dataset_file = "../generated_data/train/sample_proteins_dataset.txt"
-    # epoch_times = 2000
-
-    # timestamp = time.time()
-    # datetime_object = datetime.fromtimestamp(timestamp)
-    # dt_object = datetime_object.strftime('%Y%m%d_%H%M%S')
+    common_path = "../generated_data/whole_pdbs/datasets/try_tp_3"
     
-    # model_save_file = f"../generated_data/train/models/trained_model.dict.{dt_object}"
-    # train_log = f"../generated_data/train/logs/train.log.{dt_object}"
-    # print(f"model_save_file: {model_save_file}, \ntrain_log: {train_log}\n")
+    ### 1. Training
+    dataset_file = f"{common_path}/data/sample_proteins_dataset.train.txt"
+    epoch_times = 100
 
-    # train(dataset_file, model_save_file, train_log, epoch_times)
+    timestamp = time.time()
+    datetime_object = datetime.fromtimestamp(timestamp)
+    dt_object = datetime_object.strftime('%Y%m%d_%H%M%S')
+
+    model_save_file = f"{common_path}/models/trained_model.dict.{dt_object}"
+    train_log = f"{common_path}/logs/train.log.{dt_object}"
+    print(f"model_save_file: {model_save_file}, \ntrain_log: {train_log}\n")
+
+    train(dataset_file, model_save_file, train_log, epoch_times)
 
     ### 2. Prediction
-    validate_dataset_file = "../generated_data/validate/sample_proteins_dataset.validate.txt"
-    
-    model_save_file = f"../generated_data/train/models/trained_model.dict.20240111_024648"
+    validate_dataset_file = "{common_path}/data/sample_proteins_dataset.validate.txt"
 
-    predict_result_file = "../generated_data/predict_result.txt"
+    #model_save_file = f"{common_path}/models/trained_model.dict.20240111_024648"
+
+    predict_result_file = "{common_path}/result/predict_result.txt"
     predict(model_save_file, validate_dataset_file, predict_result_file)
