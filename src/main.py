46b247dcdde61d5ce0ac82acdca7d742f46dea61
FENG DUO <duoduo@Lucys-Macbook.local>
2024-01-27 14:34:25 -0800

    Debug, omit.

--- src/main.py
+++ src/main.py
@@ -12,11 +12,11 @@ from model.process import *
 
 
 if __name__ == "__main__":
-    common_path = "../generated_data/whole_pdbs/datasets/try_tp_3"
+    common_path = "../generated_data/whole_pdbs/datasets/try_cl_1000001"
     
     ### 1. Training
     dataset_file = f"{common_path}/data/sample_proteins_dataset.train.txt"
-    epoch_times = 50
+    epoch_times = 150
 
     timestamp = time.time()
     datetime_object = datetime.fromtimestamp(timestamp)
