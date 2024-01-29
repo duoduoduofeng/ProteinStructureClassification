6cca118dec472829d7a258af8256f5ae3ece5361
FENG DUO <duoduo@Lucys-Macbook.local>
2024-01-28 16:39:07 -0800

    Debug, omit.

--- src/prepare_whole_dataset/resize_dataset.py
+++ src/prepare_whole_dataset/resize_dataset.py
@@ -2,10 +2,22 @@ import sys
 import json
 import random
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def bin_stat(my_list):
+	bins = [0, 100, 200, 300, 400, 500, 1000, 2000, 5000, 100000]
+	hist, bin_edges = np.histogram(my_list, bins=bins)
+
+	for i in range(len(hist)):
+		print(f"Bucket {i+1}: Frequency: {hist[i]}, Boundaries: [{bin_edges[i]}, {bin_edges[i+1]}]")
 
 
 def count_by_dis(input_file):
 	stat_dict = {}
+	seq1_len = []
+	seq2_len = []
 
 	with open(input_file, 'r') as fin:
 		row_num = 0
@@ -20,25 +32,32 @@ def count_by_dis(input_file):
 			if dis not in stat_dict:
 				stat_dict[dis] = 0
 			stat_dict[dis] += 1
+
+			seq1_len.append(len(parts[3]))
+			seq2_len.append(len(parts[4]))
 	
 	sorted_dict = dict(sorted(stat_dict.items(), key=lambda item: item[1], reverse = True))
+
+	print(f"Start bin stat for seq1.")
+	bin_stat(seq1_len)
+	print(f"Start bin stat for seq2.")
+	bin_stat(seq2_len)
+
 	return sorted_dict
 
 
 # The amount of pairs with distance 8 is too large, sample it to the second large set size.
-def sample_largest_set(input_file, theseed = 2024):
+def sample_largest_set(input_file, output_file, theseed = 2024):
 	random.seed(theseed)
-
-	output_file = f"{input_file}.tailored"
 	
 	stat_dict = count_by_dis(input_file)
-	print(f"The statistic by distance for the input file: {json.dumps(stat_dict)}.")
+	print(f"The statistic by distance for the input file: {json.dumps(stat_dict)}.\n")
 
 	# The key with the largest amount
 	largest_key = list(stat_dict.keys())[0]
 	second_largest_key = list(stat_dict.keys())[1]
 
-	sample_size = int(stat_dict[second_largest_key] / 2)
+	sample_size = stat_dict[second_largest_key]
 	probability = float(sample_size / stat_dict[largest_key])
 
 	with open(input_file, 'r') as fin, open(output_file, 'w') as fout:
@@ -53,6 +72,10 @@ def sample_largest_set(input_file, theseed = 2024):
 			parts = line.strip().split("\t")
 			dis = int(parts[2])
 
+			# Filter too long sequences.
+			if len(parts[3]) > 500 or len(parts[4]) > 500:
+				continue
+
 			if dis != largest_key:
 				fout.write(line)
 			else: # sample from largest_key to second_largest_key
@@ -62,15 +85,13 @@ def sample_largest_set(input_file, theseed = 2024):
 					fout.write(line)
 					sample_size -= 1
 
-	os.rename(input_file, f"{input_file}.original")
-	os.rename(output_file, input_file)
-
-	new_stat_dict = count_by_dis(input_file)
-	print(f"Tailored the original file. The statistic is now: {json.dumps(new_stat_dict)}")
+	new_stat_dict = count_by_dis(output_file)
+	print(f"Tailored the original file {input_file} to {output_file}. \nThe statistic is now: {json.dumps(new_stat_dict)}")
 
 
 if __name__ == "__main__":
 	input_file = sys.argv[1]
+	output_file = sys.argv[2]
 	theseed = 2024
-	sample_largest_set(input_file, theseed)
+	sample_largest_set(input_file, output_file, theseed)
 
